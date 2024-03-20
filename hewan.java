11fb6eed17c7bd3294e10a86bcfa02c1e4be0456
MFaisalZ <40825206+MFaisalZ@users.noreply.github.com>
2024-03-20 20:46:54 +0700

    Update hewan.java

--- hewan.java
+++ hewan.java
@@ -1,1 +1,38 @@
+public abstract class hewan {
+ 
+    // Mendeklrasikan class dan method tipe abstract 
+    protected abstract void munculSuara();
+ 
+}
+
+public class kucing extends hewan {
+    
+    // Menggunakan method dari kelas induk abstrak
+    @Override
+    protected void munculSuara() {
+        System.out.println("Suara Kucing: Meow...meow..meow.");
+    }
+ 
+
+}
+public class burung extends hewan {
+ 
+    // Menggunakan method dari kelas induk abstrak
+    @Override
+    protected void munculSuara() {
+        System.out.println("Suara Burung: Cit...cit..cit.");
+    }
+ 
+}
+
+public class tampil {
+ 
+    public static void main (String[] args) {
+        hewan kucing = new kucing();
+        kucing.munculSuara();
+    
+        hewan burung = new burung();
+        burung.munculSuara();
+    }   
+}
 
